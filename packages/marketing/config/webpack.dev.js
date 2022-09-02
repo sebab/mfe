dac4a4851d8bbee099bad5342080437f850f9b62
Sebastián Bonta <sebastian.bonta@kushkipagos.com>
2022-09-02 10:16:18 -0300

    Primera version

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,29 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederetaionPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const devConfig = {
+    mode: 'development',
+    devServer:{
+        port: 8081,
+        historyApiFallback:{
+            index: 'index.html',
+        },
+    },
+    plugins: [
+        new ModuleFederetaionPlugin ({
+           name: 'marketing',
+           filename: 'remoteEntry.js',
+           exposes: {
+               './MarketingApp': './src/bootstrap'
+           },
+            shared: packageJson.dependencies,
+
+        }),
+    ],
+};
+
+module.exports = merge(commonConfig, devConfig);
+
