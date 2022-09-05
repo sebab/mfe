c93e84b5fe5ee2f09f7cd7af846b4837e8d08fed
Sebastián Bonta <sebastian.bonta@kushkipagos.com>
2022-09-05 10:11:37 -0300

    Randamo CSS ClassNames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import MarketingApp from "./components/MarketingApp";
+import {BrowserRouter} from "react-router-dom";
+import Header from './components/Header';
+import {createGenerateClassName, StylesProvider} from "@material-ui/core";
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 
 export default () => {
-    return <siv>
-        <h1>Hi there!</h1>
-        <hr />
-        <MarketingApp />
-    </siv>
+    return (
+        <BrowserRouter>
+            <StylesProvider generateClassName ={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+                </StylesProvider>
+        </BrowserRouter>
+    )
 };
